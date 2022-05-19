58fa4e60b5c63a7fbec9b193a20ac344a6e6a210
finnp920 <finnp920@gmail.com>
2022-05-19 17:22:57 +0900

    #10.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 function Food({ name, picture }) {
   return (
     <div>
-      <h3>{name}</h3>
-      <img src={picture} alt={name} />
+      <h3>I Like {name}</h3>
+      <img src={picture} alt={name} width="200" />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
   },
   {
+    id: 2,
     name: "bulgogi",
     image: "https://recipe1.ezmember.co.kr/cache/recipe/2019/03/03/11baafbe81803965b17c3ab42a5992cb1.jpg",
   },
   {
+    id: 3,
     name: "kimbap",
     image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
   },
   {
+    id: 4,
     name: "samgyetang",
     image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
   },
 ];
 
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+}
+
 function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-      {foodILike.map((dish) => {
-        return <Food name={dish.name} picture={dish.image} />;
-      })}
-    </div>
-  );
+  return <div>{foodILike.map(renderFood)}</div>;
 }
 
 export default App;
