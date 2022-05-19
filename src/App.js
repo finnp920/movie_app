02b8eddcd581aee49fda155e7820353df353dfd2
finnp920 <finnp920@gmail.com>
2022-05-19 15:58:14 +0900

    #10.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+function Food({ fav }) {
+  console.log(fav);
+  return <h3>{fav}</h3>;
+}
+
 function App() {
   return (
     <div>
       <h1>hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="sam" />
     </div>
   );
 }
