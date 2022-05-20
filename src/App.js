2944e9a8fb6e6c68a1467dfcd3a8a8d9bcabad56
finnp920 <finnp920@gmail.com>
2022-05-20 22:13:38 +0900

    #14.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <div>something</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
