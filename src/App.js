2a7204be3f306697d8aa7d67ddfd69a3e3c2584e
finnp920 <finnp920@gmail.com>
2022-05-20 22:26:53 +0900

    #14.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
 import About from "./routes/About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
+      <Navigation />
       <Routes>
         <Route path="/" exact={true} element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <div>About this page: I built it because I like movies.</div>;
+  return (
+    <div className="about__container">
+      <span>“Freedom is the freedom to say that two plus two make four. If that is granted, all else follows.”</span>
+      <span>− George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
